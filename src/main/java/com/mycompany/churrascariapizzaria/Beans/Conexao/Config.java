253dfafd0180307ae0bc874202a943e981e67b3b
Eduardo <Eduardofreitasfr@gmail.com>
2024-03-12 06:44:23 -0300

    Adicionado pagina de login html/css

---
+++ src/main/java/com/mycompany/churrascariapizzaria/Beans/Conexao/Config.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.churrascariapizzaria.Beans.Conexao;
+
+import static com.mycompany.churrascariapizzaria.Beans.Conexao.Config.UsuarioDB;
+
+/**
+ *
+ * @author SYNC
+ */
+public class Config {
+
+    
+    /**
+    *
+    * @author SYNC
+    *   Variaveis Globais do Banco de dados.
+    * Não alterer os nomes das variaveis
+    *   Configuração do Banco de dados
+    * Altere os valores de acordo com seu banco de dados
+    */
+    public static String UsuarioDB = "root";
+    public static String PasswordDB = "";
+    public static String DB = "Pizzaria";
+    public static String IP = "127.0.0.1";
+
+    
+}
---
+++ src/main/java/com/mycompany/churrascariapizzaria/Beans/SessaoBeans.java
@@ -0,0 +1,61 @@
+
+package com.mycompany.churrascariapizzaria.Beans;
+
+/**
+ *
+ * @author SYNC
+ */
+public class SessaoBeans {
+    private String Usuario;
+    private String Senha;
+    private boolean Authenticar;
+    private boolean UsuarioVerificado;
+    private boolean PasswordVerificado;
+    
+    public SessaoBeans(String Login, String Password){
+        this.Usuario = Login;
+        this.Senha = Password;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    public boolean isAuthenticar() {
+        return Authenticar;
+    }
+
+    public void setAuthenticar(boolean Authenticar) {
+        this.Authenticar = Authenticar;
+    }
+
+    public boolean isUsuarioVerificado() {
+        return UsuarioVerificado;
+    }
+
+    public void setUsuarioVerificado(boolean UsuarioVerificado) {
+        this.UsuarioVerificado = UsuarioVerificado;
+    }
+
+    public boolean isPasswordVerificado() {
+        return PasswordVerificado;
+    }
+
+    public void setPasswordVerificado(boolean PasswordVerificado) {
+        this.PasswordVerificado = PasswordVerificado;
+    }
+    
+    
+}
